879792911bfa2a4830888413fd0c3b5133371fca
FacundoPiriz17 <facundo.piriz@correo.ucu.edu.uy>
2025-03-26 18:55:25 -0300

    Terminado

--- UT1-TA3/src/ContadorDePalabras.java
+++ UT1-TA3/src/ContadorDePalabras.java
@@ -3,9 +3,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ContadorDePalabras {
-
+    /**
+     * Cuenta el número de vocales, consonantes y palabras en la frase.
+     */
     public int[] cuentaPalabras(String arg) {
         int cuentaVocales = 0;
         int cuentaConsonantes = 0;
@@ -19,9 +22,9 @@ public class ContadorDePalabras {
                 if (vocales.contains(arg.charAt(i) + "")){
                     cuentaVocales++;
                 }
-            else{
-            cuentaConsonantes++;
-            }
+                else{
+                    cuentaConsonantes++;
+                }
             }
             if (arg.charAt(i) == ' ' || i == arg.length()-1)
             {
@@ -35,6 +38,9 @@ public class ContadorDePalabras {
         return new int[]{cuentaVocales, cuentaConsonantes, contador};
     }
 
+    /**
+     * Lee un archivo especificado y agrega el contenido de cada línea a un array de Strings.
+     */
     public String[] obtenerLineas(String archivos) {
         FileReader fr = null;
         ArrayList<String> lista = new ArrayList<>();
@@ -43,6 +49,10 @@ public class ContadorDePalabras {
             fr = new FileReader(archivos);
             BufferedReader br = new BufferedReader(fr);
             String lineaActual = br.readLine();
+            /*
+             * Mientras no haya llegado al fin del archivo,
+             * lee y agrega la línea al array
+             */
             while (lineaActual != null) {
                 lista.add(lineaActual);
                 lineaActual = br.readLine();
@@ -57,6 +67,10 @@ public class ContadorDePalabras {
 
     }
 
+
+    /**
+     * Recibe un array de Strings y cuenta cuántas son palabras
+     */
     public int cantPalabras(String[] lineasArchivo) {
         int contador = 0;
         for (int i = 0; i < lineasArchivo.length - 1; i++) {
@@ -67,6 +81,35 @@ public class ContadorDePalabras {
         return contador;
     }
 
+    /**
+     * Obtiene la cantidad de palabras con largo mayor al especificado en el parámetro 'largo'
+     */
+    public int largoPalabra(String frase, int largo) {
+        int contador = 0;
+        int longitudPalabra = 0;
+
+        for (int i = 0; i < frase.length(); i++) {
+            char letra = frase.charAt(i);
+            if (Character.isLetter(letra)) {
+                longitudPalabra++;
+            }
+            else {
+                if (longitudPalabra > largo) {
+                    contador++;
+                }
+                longitudPalabra = 0;
+
+            }
+        }
+        if (longitudPalabra > largo) {
+            contador++;
+        }
+        return contador;
+    }
+
+    /**
+     * Método auxiliar encargado de verificar
+     */
     private boolean esPalabra(String palabra) {
         for (int i =0; i < palabra.length(); i++) {
             if (Character.isLetter(palabra.charAt(i))) {
@@ -74,5 +117,38 @@ public class ContadorDePalabras {
             }
         }
         return false;
+    }
+
+    /**
+     * Recibe dos vectores de palabras (Strings). Itera sobre la cantidad de palabras que contiene el vector
+     */
+    public String[] palabrasComunes(String[] palabras1, String[] palabras2)
+    {
+        ArrayList<String> reps = new ArrayList<>();
+        String[] palabras1Separado = separarLineas(palabras1);
+        String[] palabras2Separado = separarLineas(palabras2);
+        for (String p1 : palabras1Separado) {
+            for (String p2 : palabras2Separado) {
+                if ((p1.equals(p2)) && (esPalabra(p1)) && (!reps.contains(p1))) {
+                    reps.add(p1);
+                }
+            }
+        }
+
+        return reps.toArray(new String[reps.size()]);
+    }
+
+    /**
+     * Método auxiliar. Recibe un vector de Strings y
+     * separa las palabras por espacio
+     */
+    private String[] separarLineas(String[] lista) {
+        ArrayList<String> listaSeparadas = new ArrayList<>();
+
+        for (String linea : lista) {
+            String[] lineaSep = linea.split(" ");
+            listaSeparadas.addAll(Arrays.asList(lineaSep));
+        }
+        return listaSeparadas.toArray(new String[listaSeparadas.size()]);
     }
 }
--- UT1-TA3/src/Principal.java
+++ UT1-TA3/src/Principal.java
@@ -1,24 +1,60 @@
+
 public class Principal {
     public static void main(String[] args) {
-        ContadorDePalabras prueba = new ContadorDePalabras();
-        int[] prueba1 = prueba.cuentaPalabras("Hola Mundo");
-        int[] prueba2 = prueba.cuentaPalabras("Hola 1111");
-        int[] prueba3 = prueba.cuentaPalabras("Hola 1AB2");
+        ContadorDePalabras contadorPalabras = new ContadorDePalabras();
 
-        for (int i = 0; i < prueba1.length; i++)
-        {
-            System.out.println(prueba1[i] + " ");
+        String rutaArchivo = args[0];
+        int[] prueba1 = contadorPalabras.cuentaPalabras("Hola mundo");
+        int[] prueba2 = contadorPalabras.cuentaPalabras("Hola 1111");
+        int[] prueba3 = contadorPalabras.cuentaPalabras("Hola 1ab1");
+        int pruebaLargo = contadorPalabras.largoPalabra("prueba de largo", 3);
+        String[] palabrasArchivo = contadorPalabras.obtenerLineas(rutaArchivo);
+        int cantPalabrasArchivo = contadorPalabras.cantPalabras(palabrasArchivo);
+
+        int largo = 3;
+        System.out.println("Palabras con mas de " + largo + " caracteres: "  + pruebaLargo);
+
+        System.out.println("V | C | + |");
+
+        for (int j : prueba1) { //(int i = 0; i < prueba1.length; i++)
+            System.out.print(j + " | ");
         }
 
-        for (int i = 0; i < prueba2.length; i++)
-        {
-            System.out.println(prueba2[i] + " ");
+        System.out.println();
+
+        for (int j : prueba2) {
+            System.out.print(j + " | ");
         }
 
-        for (int i = 0; i < prueba3.length; i++)
-        {
-            System.out.println(prueba3[i] + " ");
+        System.out.println();
+
+        for (int j : prueba3) {
+            System.out.print(j + " | ");
         }
+
+        System.out.println();
+
+        for (String palabra : palabrasArchivo) {
+            System.out.println(palabra + " | ");
+        }
+
+        System.out.print("Cantidad de palabras en el archivo: " + cantPalabrasArchivo);
+        String rutaArchivo1 = args[1];
+        String rutaArchivo2 = args[2];
+        String[] palabrasArchivo1 = contadorPalabras.obtenerLineas(rutaArchivo1);
+        for (String palabra : palabrasArchivo1) {
+            System.out.println(palabra + " | ");
+        }
+        String[] palabrasArchivo2 = contadorPalabras.obtenerLineas(rutaArchivo2);
+        for (String palabra : palabrasArchivo2) {
+            System.out.println(palabra + " | ");
+        }
+        String[] palabrasComunes = contadorPalabras.palabrasComunes(palabrasArchivo1, palabrasArchivo2);
+        for (String palabra : palabrasComunes) {
+            System.out.println(palabra);
+        }
+        System.out.println("La cantidad de palabras comunes entre los 2 textos es: " + palabrasComunes.length);
+
 
     }
 }
